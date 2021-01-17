c47ecc78b17bdeb8bbf09b87b72b426b8fc30d7f
zhaowl <3531727538@qq.com>
2021-01-17 18:58:11 +0800

    stream

--- src/main/java/cn/zhao/pojo/User.java
+++ src/main/java/cn/zhao/pojo/User.java
@@ -1,14 +1,18 @@
 package cn.zhao.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @Author zhaowl
  * @Date 2021/1/17 15:50
  * @Version 1.0
- * @Description
+ * @Description User类
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private String name;
     private Integer age;
--- src/main/java/cn/zhao/stream/StreamDemo.java
+++ src/main/java/cn/zhao/stream/StreamDemo.java
@@ -1,11 +1,33 @@
 package cn.zhao.stream;
 
+import cn.zhao.pojo.User;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * @Author zhaowl
  * @Date 2021/1/17 18:14
  * @Version 1.0
- * @Description
+ * @Description Stream
  */
 public class StreamDemo {
 
+    public static void main(String[] args) {
+        User user1 = new User("yuwl", 28, "003");
+        User user2 = new User("wuyz", 22, "002");
+        User user3 = new User("chengx", 32, "001");
+        User user4 = new User("zhouxc", 37, "004");
+
+        List<User> users = Arrays.asList(user1, user2, user3, user4);
+        users.stream()
+                .filter(user -> {
+                    user.setName(user.getName().toUpperCase());
+                    return user.getAge() <= 50;
+                })
+                .sorted(Comparator.comparing(User::getNo))
+                .limit(4)
+                .forEach(System.out::println);
+    }
 }
